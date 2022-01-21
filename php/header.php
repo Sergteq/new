b6faaea3bc0fac07e34ca8ea805b8be65eacbcb9
Sergteq <97908286+Sergteq@users.noreply.github.com>
2022-01-22 02:59:42 +0300

    test
    test

---
+++ php/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>One-lesson-NIX</title>
+</head>
+<body>
+<div class="conteiner-top">
+    <div class="top-box">
+<ul>
+    <li>
+       <div class="text"><a  href="index.php" id="home">Home</a></div>
+        </li>
+    <li><div class="text-ul">One-lesson-NIX</div></li>
+
+</ul>
+
+
+
+    </div>
+</div>
+</body>
+</html>
